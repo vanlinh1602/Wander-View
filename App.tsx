e05c5bfc329c008e2c46d54331a1e9c64c792e8f
Nguyễn Văn Linh <vanlinh14121@gmail.com>
2023-04-12 23:53:36 +0700

    add native bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Home, Settings } from './src/screens';
+/* eslint-disable react/no-unstable-nested-components */
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import { Provider } from 'react-redux';
 import store from './src/redux';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SimpleLineIcons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { NavigationContainer } from '@react-navigation/native';
+import { Home, Settings } from './src/screens';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,9 +17,49 @@ function App() {
     <Provider store={store}>
       <NativeBaseProvider>
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Settings" component={Settings} />
+          <Tab.Navigator
+            screenOptions={{
+              headerShown: false,
+              tabBarShowLabel: false,
+              tabBarActiveTintColor: '#5A4CBB',
+              tabBarStyle: { backgroundColor: '#FCFBFE', borderTopWidth: 0 },
+            }}>
+            <Tab.Screen
+              name="home"
+              component={Home}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <AntDesign name="home" color={color} size={size} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="location"
+              component={Settings}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <SimpleLineIcons name="grid" color={color} size={size} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Like"
+              component={Settings}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <SimpleLineIcons name="heart" color={color} size={size} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Bag"
+              component={Settings}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <AntDesign name="cloudo" color={color} size={size} />
+                ),
+              }}
+            />
           </Tab.Navigator>
         </NavigationContainer>
       </NativeBaseProvider>

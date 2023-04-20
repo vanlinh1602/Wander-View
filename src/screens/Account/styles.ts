6e15601a08661cc97a1494ea9532980656513e85
Nguyễn Văn Linh <vanlinh14121@gmail.com>
2023-04-20 23:42:11 +0700

    remove cmt

--- src/screens/Account/styles.ts
+++ src/screens/Account/styles.ts
@@ -1,7 +1,5 @@
 import { StyleSheet } from 'react-native';
 
-// Style should be separated into a separate file so that it can be reused many times on 1 screen
-
 export default StyleSheet.create({
   container: {
     backgroundColor: '#52ab72',
